f99f15e0cf86efa97765cd3c93b6fe94eae68b95
neuronadine <126725763+neuronadine@users.noreply.github.com>
2024-10-21 21:02:00 -0400

    refactor : univariate gaussian formula

--- utils/distributions.py
+++ utils/distributions.py
@@ -3,7 +3,8 @@ import numpy as np
 
 # Univariate Gaussian
 def univariate_gaussian(x, mu, sigma):
-    return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-0.5 * ((x - mu) / sigma) ** 2)
+    assert sigma > 0.0
+    return (1 / (sigma * np.sqrt(2 * np.pi))) * np.exp(-1/5 * ((x - mu) / sigma) ** 2)
 
 
 # Multivariate Gaussian
