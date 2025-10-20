2b3c15bdeba6de9b838ed67f6cc620aa103e2b45
neuronadine <126725763+neuronadine@users.noreply.github.com>
2025-10-19 21:56:55 -0400

    Remove unused utility files and functions related to dataset handling, distance metrics, distributions, error metrics, kernels, projections, randomization, and statistics. This cleanup enhances code maintainability and reduces clutter in the project.

--- utils/kernels.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-# Gaussian (RBF) kernel function used for Soft Parzen
-def gaussian_kernel(distances, sigma):
-    return np.exp(-(distances**2) / (2 * sigma**2))
--- utils/randomization.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-def draw_rand_label(x, label_list):
-    seed = abs(np.sum(x))
-    while seed < 1:
-        seed = 10 * seed
-    seed = int(1000000 * seed)
-    np.random.seed(seed)
-    return np.random.choice(label_list)
